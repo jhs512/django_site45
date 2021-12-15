e892b090035e2eb11f236e31975a44abdb51e1cd
jhs512 <jangka512@gmail.com>
2021-12-15 12:01:23 +0900

    데이터 저장

--- pybo/views.py
+++ pybo/views.py
@@ -1,9 +1,10 @@
 import string
 
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
-from .models import Question
+from .models import Question, Answer
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -16,8 +17,14 @@ def index(request: HttpRequest) -> HttpResponse:
 
 def detail(request: HttpRequest, question_id: int) -> HttpResponse:
     # question = Question.objects.get(id=question_id)
-    question = get_object_or_404(Question, id=question_id)
+    question: Question = get_object_or_404(Question, id=question_id)
 
     return render(request, "pybo/question_detail.html", {
         "question": question
     })
+
+
+def answer_create(request: HttpRequest, question_id: int) -> HttpResponse:
+    question: Question = get_object_or_404(Question, id=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)

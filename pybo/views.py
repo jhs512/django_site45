8bf3e2f82968d868924bdcfc463d50bb2c44410e
jhs512 <jangka512@gmail.com>
2021-12-15 11:20:10 +0900

    질문에 대한 리스트, 디테일 템플릿 작업

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,23 @@
+import string
+
 from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("안녕")
+    question_list = Question.objects.order_by('-id')
+
+    return render(request, "pybo/question_list.html", {
+        "question_list": question_list
+    })
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, id=question_id)
+
+    return render(request, "pybo/question_detail.html", {
+        "question": question
+    })
